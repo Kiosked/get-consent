c5b49499afe410488f23a75082396f2abde79d85
Perry Mitchell <perry@kiosked.com>
2018-09-18 13:01:53 +0300

    Add first implementation

---
+++ source/ConsentStringFetcher.js
@@ -0,0 +1,204 @@
+import { startTimer, stopTimer } from "./timer.js";
+
+const CALLBACKS = [
+    "cmpDetected",
+    "consentData",
+    "consentString"
+];
+
+/**
+ * Timing values for checking if the __cmp() method is available
+ */
+const CMP_CHECK_TIMINGS = [
+    50,
+    100,
+    "2x200",
+    "3x300",
+    "10x500",
+    "10x750",
+    1000
+];
+
+/**
+ * GDPR consent data
+ * @typedef {Object} CMPConsentData
+ * @property {String} consentData - GDPR consent string
+ * @property {Boolean} gdprApplies - Whether GDPR applies in the current context
+ * @property {Boolean} hasGlobalScope - "true if the vendor consent data is retrieved from the global cookie, false if from a publisher-specific (or publisher-group-specific) cookie"
+ */
+
+/**
+ * Check if a consent payload object is valid
+ * @param {Object} payload The GDPR consent payload from a __cmp() call
+ * @returns {Boolean} True if valid, false otherwise
+ * @private
+ * @static
+ */
+function consentPayloadValid(payload) {
+    if (typeof payload !== "object" || payload === null) {
+        return false;
+    } else if (!hasProperty(payload, "consentData") || !hasProperty(payload, "gdprApplies")) {
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Check if an object has a property
+ * @param {Object} obj
+ * @param {*} propertyName
+ */
+function hasProperty(obj, propertyName) {
+    return obj.hasOwnProperty(propertyName) || Object.keys(obj).indexOf(propertyName) >= 0;
+}
+
+/**
+ * Initialise the fetcher
+ * @param {ConsentStringFetcher} fetcher The fetcher instance
+ * @see https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/CMP%20JS%20API%20v1.1%20Final.md#what-api-will-need-to-be-provided-by-the-cmp-
+ * @private
+ * @static
+ */
+function initFetcher(fetcher) {
+    const win = fetcher._window;
+    let timer;
+    const checkCMP = () => {
+        if (typeof win.__cmp === "function") {
+            fetcher._fireCallback("cmpDetected", null);
+            stop(timer);
+            win.__cmp("getConsentData", null, (consentPayload, wasSuccessful) => {
+                if (!wasSuccessful || !consentPayloadValid(consentPayload)) {
+                    return;
+                }
+                fetcher._lastSuccessfulData = Object.assign({}, consentPayload);
+                fetcher._fireCallback("consentData", consentPayload);
+                fetcher._fireCallback("consentString", consentPayload.consentData);
+            });
+        }
+    };
+    timer = startTimer(checkCMP, CMP_CHECK_TIMINGS);
+}
+
+/**
+ * Consent String fetcher
+ */
+export default class ConsentStringFetcher {
+    /**
+     * Create a fetcher attached to a window
+     * @param {Window=} win The window to attach to
+     * @returns {ConsentStringFetcher} New fetcher instance
+     * @static
+     * @memberof ConsentStringFetcher
+     */
+    static attachToWindow(win = window) {
+        return new ConsentStringFetcher(win);
+    }
+
+    /**
+     * Constructor for the fetcher
+     * @param {Window} win The window instance (required)
+     * @memberof ConsentStringFetcher
+     */
+    constructor(win) {
+        if (!win) {
+            throw new Error("First parameter is required (window)");
+        }
+        this._window = win;
+        this._lastSuccessfulData = null;
+        this._callbacks = CALLBACKS.reduce((out, cbName) => ({ ...out, [cbName]: [] }), {});
+        initFetcher(this);
+    }
+
+    /**
+     * The last successfully fetched consent data object
+     * @type {CMPConsentData|null}
+     * @memberof ConsentStringFetcher
+     * @readonly
+     */
+    get consentData() {
+        return this._lastSuccessfulData
+            ? Object.freeze(this._lastSuccessfulData)
+            : null;
+    }
+
+    /**
+     * Remove event listener
+     * @param {String} eventType The event type that was being listened to
+     * @param {Function} callback The callback function that was attached
+     * @memberof ConsentStringFetcher
+     */
+    off(eventType, callback) {
+        const ind = this._callbacks[eventType].indexOf(callback);
+        if (ind >= 0) {
+            this._callbacks[eventType].splice(ind, 1);
+        }
+    }
+
+    /**
+     * .on() return value
+     * @typedef OnMethodReturnValue
+     * @property {Function} remove - Remove method to cancel the listener
+     */
+
+    /**
+     * Attach an event
+     * @param {*} eventType The type of event to listen to
+     * @param {*} callback The callback to fire when the event is triggered
+     * @returns {OnMethodReturnValue} Controls for the listener
+     * @memberof ConsentStringFetcher
+     */
+    on(eventType, callback) {
+        this._callbacks[eventType].push(callback);
+        return {
+            remove: () => this.off(eventType, callback)
+        };
+    }
+
+    /**
+     * Wait for consent data
+     * @returns {Promise.<CMPConsentData>} Promise that resolves with consent data
+     *  from the CMP system. Be aware that the promise may never resolve if consent
+     *  data is never received.
+     * @memberof ConsentStringFetcher
+     */
+    waitForConsent() {
+        return new Promise(resolve => {
+            const { remove } = this.on("consentData", data => {
+                remove();
+                return data;
+            });
+        });
+    }
+
+    /**
+     * Wait for a consent string
+     * @returns {Promise.<String>} Promise that resolves with a consent string
+     *  from the CMP system.
+     * @see waitForConsent
+     * @memberof ConsentStringFetcher
+     */
+    waitForConsentString() {
+        return this.waitForConsent().then(data => data.consentData);
+    }
+
+    /**
+     * Fire a callback
+     * @param {String} type The callback type
+     * @param {*} data The data to provide to the callback
+     * @memberof ConsentStringFetcher
+     * @protected
+     * @throws {Error} Throws if the callback type isn't recognised
+     */
+    _fireCallback(type, data) {
+        if (!this._callbacks[type]) {
+            throw new Error(`Failed firing callbacks: Unrecognised callback type: ${type}`);
+        }
+        this._callbacks[type].forEach(cb => {
+            try {
+                cb(data);
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    }
+}
---
+++ source/timer.js
@@ -0,0 +1,53 @@
+function expandTimings(timings) {
+    if (typeof timings === "number") {
+        return timings;
+    } else if (!Array.isArray) {
+        throw new Error(`Failed expanding timer timings: Value not a number or array: ${timings}`);
+    }
+    const computedTimings = [];
+    timings.forEach(raw => {
+        if (typeof raw === "number") {
+            computedTimings.push(raw);
+            return;
+        }
+        const [count, delay] = raw.split(/x/i);
+        if (!count || count <= 0 || !/^\d+$/.test(delay)) {
+            throw new Error(`Failed expanding raw timings: Expected (count)x(delay), got: ${count}x${delay}`);
+        }
+        for (let i = 0; i < parseInt(count, 10); i += 1) {
+            computedTimings.push(parseInt(delay, 10));
+        }
+    });
+    return computedTimings;
+}
+
+export function startTimer(cb, timings) {
+    const timer = {
+        enabled: true,
+        jsTimer: null
+    };
+    const timingArr = expandTimings(timings);
+    const run = () => {
+        if (!timer.enabled) {
+            return;
+        }
+        const nextTime = timingArr.length > 1
+            ? timingArr.shift()
+            : timingArr[0];
+        timer.jsTimer = setTimeout(() => {
+            try {
+                cb();
+            } catch (err) {
+                console.error(err);
+            }
+            run();
+        }, nextTime);
+    };
+    run();
+    return timer;
+}
+
+export function stopTimer(timer) {
+    timer.enabled = false;
+    clearTimeout(timer.jsTimer);
+}
