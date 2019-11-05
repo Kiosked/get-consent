f7ac7786792872a849c342cfa94d19ac9c7bf3e1
Perry Mitchell <perry@kiosked.com>
2019-11-05 13:38:28 +0200

    Update docs

--- source/timeout.js
+++ source/timeout.js
@@ -1,4 +1,18 @@
-export function timeoutPromise(promise, timeout, errorMsg = "Timed-out waiting for result") {
+const DEFAULT_ERROR_MESSAGE = "Timed-out waiting for result";
+
+/**
+ * Wrap a promise with a time-out that rejects the promise
+ * chain if the desired time is reached
+ * @param {Promise} promise The promise instance
+ * @param {Number} timeout The timeout in milliseconds
+ * @param {String=} errorMsg Error message for time-outs
+ * @returns {Promise} A wrapped promise instance
+ * @throws {TimeoutError} Throws if the promise execution
+ *  times out
+ * @private
+ * @static
+ */
+export function timeoutPromise(promise, timeout, errorMsg = DEFAULT_ERROR_MESSAGE) {
     return new Promise((resolve, reject) => {
         promise
             .then(result => {
